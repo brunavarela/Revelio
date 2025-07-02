69a314504690178852df1c62a9854ad3674a3f2c
Bruna Varela <brunavarela1@hotmail.com>
2025-07-02 19:36:34 -0300

    feat: first commit

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import './global.css';
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <ScreenContent></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
